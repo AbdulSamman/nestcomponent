9e01c4782da45eed13c69e23c74225564db3cdbd
AbdulSamman <sammanab@outlook.de>
2024-01-27 03:22:10 +0100

    data is in

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.scss'
+import './App.scss';
+import Books from "./data/books.json";
 
 function App() {
-
-
   return (
     <div>
      <h1>Book Site</h1>
-     <p>testing</p>
+     <h3>There are {Books.length} books.</h3>
+     
+      <div>{Books.map((book,index)=>{
+        return (
+          <div className='book' key={index}>
+            <div className='title'>{book.title}</div>
+            <div className='imageUrl'>
+              <img src={book.imageUrl} />
+            </div>
+          </div>
+        )
+      })}</div>
+     
     </div>
   )
 }
